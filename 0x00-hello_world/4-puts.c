49401d0b9dfcdd37e7c54f0d9570d7919e62ee11
mortada12331 <m0rorder000@gmail.com>
2023-09-11 11:13:45 -0700

    alx

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
-#include <stdio.h>
-/**
- * main - Entry point
- * Description: A C programming thatprints with function 
- * Return: Always 0 (Success)
- */
-int main(void)
-{
+   #include <stdio.h>
+   /**
+   * main - Entry point
+   * Description: A C programming thatprints with function 
+   * Return: Always 0 (Success)
+   */
+   int main(void)
+   {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-}
+   }
